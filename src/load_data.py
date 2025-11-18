8052ff4c4305f376626a2a825580d0c5e2dc94df
Laxmi Prasanna Podhem <prasannapodhem@gmail.com>
2025-11-18 11:29:55 -0500

    Update load_data.py

--- src/load_data.py
+++ src/load_data.py
@@ -1,1 +1,8 @@
-# initial file
+import pandas as pd
+
+def load_and_clean_data("C:\Users\prasa\OneDrive\Documents\Term 4\Agile\financial_transactions.csv"):
+    df = pd.read_csv("C:\Users\prasa\OneDrive\Documents\Term 4\Agile\financial_transactions.csv")
+    df['date'] = pd.to_datetime(df['date'], errors='coerce')
+    df['type'] = df['type'].str.lower().str.strip()
+    return df
+
