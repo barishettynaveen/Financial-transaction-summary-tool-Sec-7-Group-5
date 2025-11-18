d6cdb08607adfff789ee9e2d649b63252b224766
Laxmi Prasanna Podhem <prasannapodhem@gmail.com>
2025-11-18 11:31:07 -0500

    Update summary_functions.py

--- src/summary_functions.py
+++ src/summary_functions.py
@@ -1,1 +1,8 @@
-# initial file
+def calculate_income_expense(df):
+    income = df[df['type'] == 'credit']['amount'].sum()
+    expense = df[df['type'] == 'debit']['amount'].sum()
+    return {"total_income": income, "total_expense": expense}
+
+def customer_summary(df):
+    return df.groupby('customer_id')['amount'].sum().sort_values(ascending=False)
+
