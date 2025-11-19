56f259b1974dececfcc4f9db0eb87189e36db8be
Laxmi Prasanna Podhem <prasannapodhem@gmail.com>
2025-11-18 21:26:22 -0500

    Update test_transactions.py

--- src/test_transactions.py
+++ src/test_transactions.py
@@ -1,16 +1,3 @@
-from src.load_data import load_and_clean_data
-from src.summary_functions import calculate_income_expense
-
-def test_load_data():
-    df = load_and_clean_data("financial_transactions.csv")
-    assert "date" in df.columns
-    assert df['date'].dtype.name.startswith("datetime")
-
-def test_income_expense():
-    df = load_and_clean_data("financial_transactions.csv")
-    result = calculate_income_expense(df)
-    assert "total_income" in result
-    assert "total_expense" in result
-
+#initial code
 
 
