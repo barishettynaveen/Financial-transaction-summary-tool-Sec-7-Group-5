ecb6ef22c0dfc76629649f641bcedb2a39c857b7
Laxmi Prasanna Podhem <prasannapodhem@gmail.com>
2025-11-20 12:09:30 -0500

    Update summary_functions.py

--- src/summary_functions.py
+++ src/summary_functions.py
@@ -1,2 +1,12 @@
-#unitial code
+def calculate_income_expense(df):
+    """
+    Calculates total income (credit) and total expenses (debit).
+    """
 
+    income = df[df["type"] == "credit"]["amount"].sum()
+    expense = df[df["type"] == "debit"]["amount"].sum()
+
+    return {
+        "total_income": income,
+        "total_expense": expense
+    }
