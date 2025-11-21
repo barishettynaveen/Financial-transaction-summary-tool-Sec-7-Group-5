72d75f18f169ef16266969a67cfd00705c2765f4
Laxmi Prasanna Podhem <prasannapodhem@gmail.com>
2025-11-21 13:56:13 -0500

    Restored US-2 and US-33 functions

--- src/summary_functions.py
+++ src/summary_functions.py
@@ -11,3 +11,9 @@ def calculate_income_expense(df):
         "total_income": income,
         "total_expense": expense
     }
+
+
+def customer_summary(df):
+    customer_totals = df.groupby("customer_id")["amount"].sum().reset_index()
+    customer_totals = customer_totals.sort_values(by="amount", ascending=False)
+    return customer_totals
